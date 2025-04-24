1fc083067505d2571e509ee6c7c78c00f1f306bb
Katie vo <S114289@ltisdschools.net>
2025-04-24 14:23:09 -0500

    set up FollowPlayer script & added in obstacles

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float smoothness;
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + offset, smoothness);
+    }
+}
